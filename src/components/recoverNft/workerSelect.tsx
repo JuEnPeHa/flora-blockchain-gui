562aec3d0419d8e20ec008810025040555170f35
JuEnPeHa <67208274+JuEnPeHa@users.noreply.github.com>
2021-09-01 22:54:43 -0500

    027

--- src/components/recoverNft/workerSelect.tsx
+++ src/components/recoverNft/workerSelect.tsx
@@ -1,7 +1,15 @@
-import {FormControl, FormControlLabel, Grid, InputLabel, MenuItem, Radio}
+import {
+  FormControl,
+  FormControlLabel,
+  Grid, InputAdornment,
+  InputLabel,
+  MenuItem,
+  Radio,
+  Typography
+}
   from '@material-ui/core';
 import React from "react";
-import {Flex, RadioGroup, Select} from "@flora/core";
+import {Flex, RadioGroup, Select, TextField} from "@flora/core";
 import { useFormContext } from 'react-hook-form';
 import { Trans } from '@lingui/macro';
 
@@ -23,7 +31,6 @@ export default function SelectWorker() {
           <MenuItem value ={3}>Soon</MenuItem>
         </Select>
       </FormControl>
-      </Grid>
 
       <FormControl variant="filled" fullWidth>
         <RadioGroup name="worker" boolean>
@@ -41,7 +48,32 @@ export default function SelectWorker() {
           </Flex>
         </RadioGroup>
       </FormControl>
+      </Grid>
 
+      {worker && (
+        <Grid xs={12} md={8} lg={6} item>
+          <FormControl variant="filled">
+            <Typography variant="subtitle1">
+              <Trans>Want to have a delay before the next plot starts?</Trans>
+            </Typography>
+            <TextField
+              name="delay"
+              type="number"
+              variant="filled"
+              label={<Trans>Delay</Trans>}
+              InputProps={{
+                inputProps: { min: 0 },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Trans>Minutes</Trans>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </FormControl>
+        </Grid>
+      )}
     </Grid>
+
 
   )}
